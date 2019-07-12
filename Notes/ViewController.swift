0b98d0019dcce081a443cefed77fe29db314b777
vzbiven <vzbiven@yandex.ru>
2019-07-12 15:55:43 +0300

    palette and flag added and works

--- Notes/ViewController.swift
+++ Notes/ViewController.swift
@@ -24,9 +24,70 @@ class ViewController: UIViewController {
         }
         
     }
+    
+    @IBOutlet weak var noteText: UITextView!
+    @IBOutlet weak var palette1: UIView!
+    @IBOutlet weak var palette2: UIView!
+    @IBOutlet weak var palette3: UIView!
+    @IBOutlet weak var palette4: UIView!
+    
+    @IBOutlet weak var flag1: SelectionFlag!
+    @IBOutlet weak var flag2: SelectionFlag!
+    @IBOutlet weak var flag3: SelectionFlag!
+    @IBOutlet weak var flag4: SelectionFlag!
+    
+    
+    @IBAction func palette1tapped(_ sender: UITapGestureRecognizer) {
+        noteText.textColor = palette1.backgroundColor
+        flag1.isHidden = false
+        flag2.isHidden = true
+        flag3.isHidden = true
+        flag4.isHidden = true
+        
+    }
+
+    @IBAction func palette2tapped(_ sender: UITapGestureRecognizer) {
+        noteText.textColor = palette2.backgroundColor
+        flag1.isHidden = true
+        flag2.isHidden = false
+        flag3.isHidden = true
+        flag4.isHidden = true
+    }
+    
+    @IBAction func palette3tapped(_ sender: UITapGestureRecognizer) {
+        noteText.textColor = palette3.backgroundColor
+        flag1.isHidden = true
+        flag2.isHidden = true
+        flag3.isHidden = false
+        flag4.isHidden = true
+    }
+    
+    @IBAction func palette4tapped(_ sender: UITapGestureRecognizer) {
+        noteText.textColor = palette4.backgroundColor
+        flag1.isHidden = true
+        flag2.isHidden = true
+        flag3.isHidden = true
+        flag4.isHidden = false
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         datePickerHeight.constant = 0
+        palette1.backgroundColor = UIColor.black
+        palette1.layer.borderWidth = 3
+        palette1.layer.borderColor = UIColor.black.cgColor
+        
+        palette2.backgroundColor = UIColor.blue
+        palette2.layer.borderWidth = 3
+        palette2.layer.borderColor = UIColor.black.cgColor
+        
+        palette3.backgroundColor = UIColor.brown
+        palette3.layer.borderWidth = 3
+        palette3.layer.borderColor = UIColor.black.cgColor
+        
+        palette4.layer.borderWidth = 3
+        palette4.layer.borderColor = UIColor.black.cgColor
+        
         
         //textView.translatesAutoresizingMaskIntoConstraints = true
         //textView.sizeToFit()
